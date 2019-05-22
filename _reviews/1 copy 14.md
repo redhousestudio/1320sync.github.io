df6c4c93df9846d086fbed55613b0158c3af4b97
Faizan Vahevaria <faizanvahevaria@gmail.com>
2019-05-22 07:52:39 +0530

    reviews section random slider with separate md files

---
+++ _reviews/1 copy 14.md
@@ -0,0 +1,6 @@
+---
+stars: 4
+country: 14United States
+title: Calendar
+description: This is your main Mayan Dreamspell Calendar Dashboard with all the details you need to stay in sync. Calculate the Kin for any date.
+---
---
+++ js/script.js
@@ -0,0 +1,59 @@
+(function($){
+ 
+  $.fn.shuffle = function() {
+
+      var allElems = this.get(),
+          getRandom = function(max) {
+              return Math.floor(Math.random() * max);
+          },
+          shuffled = $.map(allElems, function(){
+              var random = getRandom(allElems.length),
+                  randEl = $(allElems[random]).clone(true)[0];
+              allElems.splice(random, 1);
+              return randEl;
+         });
+
+      this.each(function(i){
+          $(this).replaceWith($(shuffled[i]));
+      });
+
+      return $(shuffled);
+
+  };
+
+})(jQuery);
+
+$(document).on('ready', function() {
+  $(document).click(function (event) {
+    var clickover = $(event.target);
+    var _opened = $(".navbar-collapse").hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $(".navbar-toggler").click();
+    }
+  });
+  $(".multiple-items .review-item").shuffle();
+  $('.multiple-items').slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
